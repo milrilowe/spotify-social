a69e5070734386b364bbcedb16349703d0c6a368
Miles <miles@creovia.io>
2025-01-13 14:56:24 -0700

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+This is an example project showing a large monorepo structure.
---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import typescript from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+
+export default [
+    {
+        ignores: ['**/node_modules/**', '**/dist/**', '**/coverage/**']
+    },
+        eslint.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx}'],
+        languageOptions: {
+            parser: typescript,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+            'react': react,
+            'react-hooks': reactHooks,
+            'prettier': prettier,
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'react/react-in-jsx-scope': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/no-unused-vars': ['warn', {
+                'argsIgnorePattern': '^_',
+                'varsIgnorePattern': '^_'
+            }],
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn'
+        },
+        settings: {
+            react: {
+            version: 'detect'
+            }
+        }
+    }
+];
