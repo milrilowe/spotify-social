24d17a6e684a3b1016919620eebf5db5a3446487
Miles <miles@creovia.io>
2025-01-14 10:42:46 -0700

    removed utility package bc trpc makes it unnecessary

--- packages/spotify-social/mock/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { setupWorker } from 'msw/browser'
-import { authHandlers } from './handlers/auth'
-
-export const worker = setupWorker(...authHandlers);
-
-export async function setupMock() {
-    await worker.start()
-}
