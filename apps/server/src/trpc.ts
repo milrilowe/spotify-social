fb3fc40cf3f4812b3cb21f2ec9162bd7e724c452
Miles <miles@creovia.io>
2025-01-21 11:48:37 -0700

    trying to get trpc types into client

--- apps/server/src/trpc.ts
+++ apps/server/src/trpc.ts
@@ -1,12 +1,6 @@
 import { initTRPC } from "@trpc/server";
 import type { CreateExpressContextOptions } from "@trpc/server/adapters/express";
-import type { Session } from "express-session";
-import type { SpotifyTokenResponse } from './routes/auth/types';
-
-interface AppSession extends Session {
-    spotifyTokens?: SpotifyTokenResponse;
-    userId?: string;
-}
+import type { AppSession } from './types';
 
 export type Context = {
     req: CreateExpressContextOptions["req"];
@@ -14,26 +8,9 @@ export type Context = {
     session: AppSession;
 };
 
-const t = initTRPC.context<Context>().create({
-    errorFormatter({ shape, error }) {
-        return {
-            ...shape,
-            data: {
-                ...shape.data,
-                code: error.code,
-            },
-        };
-    },
-});
-
-export const createContext = ({
-    req,
-    res,
-}: CreateExpressContextOptions): Context => ({
-    req,
-    res,
-    session: (req as any).session as AppSession
-});
+export const t = initTRPC
+    .context<Context>()
+    .create();
 
 export const router = t.router;
 export const middleware = t.middleware;
